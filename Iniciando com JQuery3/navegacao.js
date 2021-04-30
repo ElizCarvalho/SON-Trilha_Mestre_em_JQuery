22647a6381d134edcebf3038d0f25ca68805ad8a
ElizCarvalho <elizabethcarvalh0@yahoo.com>
2021-04-30 11:50:09 -0300

    Ajax

--- Iniciando com JQuery3/navegacao.js
+++ Iniciando com JQuery3/navegacao.js
@@ -6,8 +6,22 @@
 //executa assim q o carregamento do html ficar pronto 
 //(carregamento de img não seria o caso... ele executaria antes)
 $(document).ready(function(){
+
+    //Ja carrega a primeira vez sem precisar clicar
+    $("#conteudo").load('pages/home.html')
+
     //alert($('body').html())
     $('#menu a').click(function(){
-        return true; //se tiver false... mesmo com o link certinho não funcionaria (estaria block)
+        //pega o valor da propriedade .. se quiser colocar valor seria .attr('href','novo valor')
+        let url = $(this).attr('href'); 
+
+        $.ajax({
+            method: "GET",
+            url: url,
+        })
+        .done(function(response){
+            $("#conteudo").html(response);
+        });
+        return false; //se tiver false... mesmo com o link certinho não funcionaria (estaria block)
     })
 });
